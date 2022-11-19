b89686b31410f71f400feea68d3fb391bd7b3616
Stephen <survivingthequiet@gmail.com>
2022-11-19 00:04:31 +0000

    feat: aria-label updates

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Stephen Rutherford";
-export const SITE_DESCRIPTION = "Portfolio";
+export const SITE_DESCRIPTION = "Fraud Analyst & front-end developer.";
 export const SITE_WIDTH = "max-w-7xl";
